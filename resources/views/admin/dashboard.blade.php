dbd565fe83707a4ef6387aed40900fdddc797861
easylearningbd <elebdsoft@gmail.com>
2025-06-02 03:20:58 +0600

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+<html>
+    <title>Admin Dashboard</title>
+    <body>
+        
+        <h1>Admin Main Dashboard </h1>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsUser;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+/// User Routes 
+Route::middleware(['auth', IsUser::class])->group(function () {
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+});
+/// Eend User Routes 
+
+
+/// Admin Routes 
+Route::prefix('admin')->middleware(['auth', IsAdmin::class])->group(function () {
+    
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+
+});
+/// Eend User Routes 
+ 
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
