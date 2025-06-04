3e4ec65d2a935e51d9cd0cfa45c3a8c34ee23bb6
easylearningbd <elebdsoft@gmail.com>
2025-06-05 02:10:22 +0600

    29 Create Templates for Content Generate Part 4

--- resources/views/admin/backend/template/add_template.blade.php
+++ resources/views/admin/backend/template/add_template.blade.php
@@ -72,6 +72,76 @@
             </div>
         </div>
     </div>
+
+    <div class="form-group">
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked="">
+            <label class="form-check-label" for="flexCheckChecked"> Activate Template </label>
+        <input type="hidden" name="is_active" value="1">
+        </div>
+        </div>
+
+
+    <div class="form-group">
+        <div id="input-fields">
+            <div class="row input-field-row">
+              
+     <div class="col-md-3">
+      
+        <div class="form-group">
+            <label for="input_fields_0_title" class="form-label">Input Field Title * </label>
+            <div class="form-control-wrap">
+                <input type="text" name="input_fields[0][title]" id="input_fields_0_title" class="form-control" placeholder="Enter Input Field Title" required  >
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-3">
+      
+        <div class="form-group">
+            <label for="input_fields_0_description" class="form-label">Input Field Description * </label>
+            <div class="form-control-wrap">
+                <input type="text" name="input_fields[0][description]" id="input_fields_0_description" class="form-control" placeholder="Enter Input Field Description" required  >
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-3">
+      
+        <div class="form-group">
+            <label for="input_fields_0_type" class="form-label"> Field Type * </label>
+            <div class="form-control-wrap">
+      <select name="input_fields[0][type]" class="form-control" id="input_fields_0_type"  > 
+        <option value="text">Input Field</option>
+        <option value="textarea">Textarea Field</option> 
+    </select>
+            </div>
+        </div> 
+    </div>
+
+
+    <div class="col-md-3">
+      
+        <div class="form-group">
+            <label class="form-label">  </label>
+            <div class="form-control-wrap">
+      <input type="hidden" name="input_fields[0][is_required]"    value="1">
+            </div>
+        </div> 
+    </div> 
+            </div> 
+        </div>
+
+
+    <div class="form-group mt-2">
+        <label for="prompt">Custom Prompt</label>
+        <textarea placeholder="Add Your Prompt Code" class="form-control"  rows="3"></textarea>
+        <small>Write a 400 world aticale about {topic} with an introductions</small> 
+    </div>
+
+
+
+    </div>
  
  
                
