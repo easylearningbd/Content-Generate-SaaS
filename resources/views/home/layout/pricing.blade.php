db82d6519483a187e39ec576fc971c963843ea6e
easylearningbd <elebdsoft@gmail.com>
2025-06-21 00:38:11 +0600

    106 Frontend Pricing Section Setup

--- resources/views/home/layout/pricing.blade.php
+++ resources/views/home/layout/pricing.blade.php
@@ -17,66 +17,38 @@
     </div><!-- .section-head -->
     <div class="section-content">
         <div class="row g-gs justify-content-center">
-            <div class="col-lg-4 col-md-6">
-                <div class="pricing">
-                    <div class="pricing-body p-5">
-                        <div class="text-center">
-                            <h5 class="mb-3">Bronze</h5>
-                            <h3 class="h2 mb-4">$9 <span class="caption-text text-muted"> / month</span></h3>
-                            <a href="#" class="btn btn-primary btn-soft btn-block">Start free trial today</a>
-                        </div>
-                        <ul class="list gy-3 pt-4 ps-2">
-                            <li><em class="icon ni ni-check text-success"></em> <span><strong>10,000</strong> Monthly Word Limit</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span><strong>10+</strong> Templates</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>30+ Languages</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Advance Editor Tool</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Regular Technical Support</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Unlimited Logins</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Newest Features</span></li>
-                        </ul>
-                    </div>
-                </div>
-            </div><!-- .col -->
-            <div class="col-lg-4 col-md-6">
-                <div class="pricing pricing-featured bg-primary">
-                    <div class="pricing-body p-5">
-                        <div class="text-center">
-                            <h5 class="mb-3">Silver</h5>
-                            <h3 class="h2 mb-4">$19 <span class="caption-text text-muted"> / month</span></h3>
-                            <a href="#" class="btn btn-primary btn-block">Start free trial today</a>
-                        </div>
-                        <ul class="list gy-3 pt-4 ps-2">
-                            <li><em class="icon ni ni-check text-success"></em> <span><strong>20,000</strong> Monthly Word Limit</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span><strong>10+</strong> Templates</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>50+ Languages</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Advance Editor Tool</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Regular Technical Support</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Unlimited Logins</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Newest Features</span></li>
-                        </ul>
-                    </div>
-                </div>
-            </div><!-- .col -->
-            <div class="col-lg-4 col-md-6">
-                <div class="pricing">
-                    <div class="pricing-body p-5">
-                        <div class="text-center">
-                            <h5 class="mb-3">Diamond</h5>
-                            <h3 class="h2 mb-4">$39 <span class="caption-text text-muted"> / month</span></h3>
-                            <a href="#" class="btn btn-primary btn-soft btn-block">Start free trial today</a>
-                        </div>
-                        <ul class="list gy-3 pt-4 ps-2">
-                            <li><em class="icon ni ni-check text-success"></em> <span><strong>50,000</strong> Monthly Word Limit</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span><strong>15+</strong> Templates</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>70+ Languages</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Advance Editor Tool</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Regular Technical Support</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Unlimited Logins</span></li>
-                            <li><em class="icon ni ni-check text-success"></em> <span>Newest Features</span></li>
-                        </ul>
-                    </div>
-                </div>
-            </div><!-- .col -->
+            
+            
+  @php
+      $plan = App\Models\Plan::orderBy('id','asc')->get();
+      $silverPlan = $plan->where('name','Silver')->first();
+  @endphp          
+
+@foreach ($plan as $planItem) 
+
+<div class="col-lg-4 col-md-6">
+    <div class="pricing {{ $planItem->name === 'Silver' ? 'pricing-featured bg-primary' : 'pricing' }}  ">
+        <div class="pricing-body p-5">
+            <div class="text-center">
+                <h5 class="mb-3">{{ $planItem->name }}</h5>
+                <h3 class="h2 mb-4">${{ $planItem->price }} <span class="caption-text text-muted"> / month</span></h3>
+                <a href="{{ route('register') }}" class="btn {{ $planItem->name === 'Silver' ? 'btn-primary btn-block' : 'btn-primary btn-soft btn-block' }} ">Start free trial today</a>
+            </div>
+            <ul class="list gy-3 pt-4 ps-2">
+                <li><em class="icon ni ni-check text-success"></em> <span><strong>{{ $planItem->monthly_word_limit }}</strong> Monthly Word Limit</span></li>
+                <li><em class="icon ni ni-check text-success"></em> <span><strong>{{ $planItem->templates }}+</strong> Templates</span></li>
+                <li><em class="icon ni ni-check text-success"></em> <span>6+ Languages</span></li>
+                <li><em class="icon ni ni-check text-success"></em> <span>Advance Editor Tool</span></li>
+                <li><em class="icon ni ni-check text-success"></em> <span>Regular Technical Support</span></li>
+                <li><em class="icon ni ni-check text-success"></em> <span>Unlimited Logins</span></li>
+                <li><em class="icon ni ni-check text-success"></em> <span>Newest Features</span></li>
+            </ul>
+        </div>
+    </div>
+</div><!-- .col -->
+@endforeach
+
+            
         </div><!-- .row -->
     </div><!-- .section-content -->
 </div><!-- .container -->
